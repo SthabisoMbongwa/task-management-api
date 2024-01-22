7e658e8656fcab27d7427db20781599c73e92644
Sthabiso <sthabiso@aims.ac.za>
2024-01-22 11:33:00 +0200

    Created and saved the task

--- TaskManagerBackend/routes/taskRoutes.js
+++ TaskManagerBackend/routes/taskRoutes.js
@@ -17,8 +17,18 @@ router.get('/test',auth, (req, res) => {
 //Create a task
 router.post('/', auth, async (req, res) => {
     try{
-
+        //description, completed from req.body
+        //owner : req.user._id
+        const task = new Task({
+            ...req.body, 
+            owner: req.user._id
+        });
+        await task.save();
+        res.status(201).json({task, message: "Task Created Successfully!"});
     }
-})
+    catch(err){
+        res.status(400).send({error: err});
+    }
+});
 
 module.exports = router;
