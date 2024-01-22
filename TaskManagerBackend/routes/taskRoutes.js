ac705118733216bf6aa5bdb8f0742e7c700f9fb0
Sthabiso <sthabiso@aims.ac.za>
2024-01-22 10:29:59 +0200

    Created a middleware for authentication

--- TaskManagerBackend/routes/taskRoutes.js
+++ TaskManagerBackend/routes/taskRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 
 const router = express.Router();
+const auth = require('../middlewares/auth')
 
-router.get('/', (req, res) => {
-    res.send("Task routes are working!")
-})
+router.get('/test',auth, (req, res) => {
+    res.send("Task routes are working!");
+});
 
 //CRUD tasks
 module.exports = router;
