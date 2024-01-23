8515b92e5fb26920c3911e09d3d338781a24b323
Sthabiso <sthabiso@aims.ac.za>
2024-01-23 09:44:12 +0200

    My first commit of the day

--- TaskManagerBackend/routes/taskRoutes.js
+++ TaskManagerBackend/routes/taskRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 
 const router = express.Router();
-const auth = require('../middlewares/auth')
+const auth = require('../middlewares/auth');
 const Task = require('../models/Task');
 
 router.get('/test',auth, (req, res) => {
