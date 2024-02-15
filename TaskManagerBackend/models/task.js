6a9f69a88022912d93ea1727765df575a8c395a6
Sthabiso <sthabiso@aims.ac.za>
2024-02-15 12:40:26 +0200

    Modified my task schema, added priority, due date, category, reminder and shared_with

--- TaskManagerBackend/models/task.js
+++ TaskManagerBackend/models/task.js
@@ -1,19 +1,24 @@
 const mongoose = require('mongoose');
 
-
 const taskSchema = new mongoose.Schema({
-    description: {type: String, required: true },
-    completed: {type: Boolean, default: false},
-    owner:{
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, enum: ['Work', 'Personal', 'Shopping'], required: true },
+    priority: { type: String, enum: ['Low', 'Medium', 'High'], required: true },
+    due_date: { type: Date, required: true },
+    status: { type: String, enum: ['Todo', 'InProgress', 'Complete'], default: 'Todo' },
+    completed: { type: Boolean, default: false },
+    owner: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'User'
-    }
+    },
+    shared_with: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    reminder: { type: Date }
 }, {
-    timestamps: true
+    timestamps: { createdAt: 'created_date', updatedAt: 'updated_date' }
 });
 
-
-
 const Task = mongoose.model('Task', taskSchema);
+
 module.exports = Task;
--- TaskManagerBackend/routes/taskRoutes.js
+++ TaskManagerBackend/routes/taskRoutes.js
@@ -44,47 +44,4 @@ router.get('/', auth, async(req, res) => {
     }
 });
 
-//fetch a task by id
-
-router.get('/:id', auth, async (req, res) => {
-    const taskid = req.params.id;
-
-    try{
-          const task = await Task.findOne({
-            _id: taskid,
-            owner: req.user_id
-          });
-          if(!task){
-            return res.status(404).json({message: "Task not found"});
-          }
-          res.status(200).json({task, message: "Task Fetched Successfully"});
-    }
-    catch(err){
-        res.status(500).send({error: err});
-    }
-})
-
-
-//Update a task by id - description, completed field
-router.patch('/:id', auth, async (req, res) => {
-    const taskid = req.params.id; 
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['description', 'completed'];
-    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
-
-    if(!isValidOperation){
-        return res.status(400).json({error: "Invalid Updates"});
-    }
-
-    try{
-          
-    }
-    catch(err){
-        res.status(500).send({error: err});
-    }
-})
-
-
 module.exports = router;
-
-
